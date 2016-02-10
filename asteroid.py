cf0593b0f7412c339f86b30d577dfed0ce70249a
Steve Wilson <swwilso1@gmail.com>
2016-02-09 22:30:02 -0700

    Fix Asteroid construction geometry.

--- asteroid.py
+++ asteroid.py
@@ -1,41 +1,101 @@
 #! /usr/bin/env python
 
+from __future__ import division
 import random
 from vector import *
 from visual_object import *
 import pygame
+import math
+
+
+def abs(x):
+    if x < 0:
+        return -1 * x
+    return x
+
+# End abs
+
 
 class Asteroid(VisualObject):
     """Asteroid class for drawing asteroids to a surface"""
 
-    def __init__(self, color, rect):
+    def __init__(self, color, rect, points=7):
         self.__color = color
         self.__rect = rect
-        points = []
         self.__vectors = []
-        for i in range(0, random.randint(5,10)):
-            points.append((random.randint(rect[0],rect[0]+rect[2]),
-                                  random.randint(rect[1],rect[1]+rect[3])))
-
-#        print points
 
         self.__center = (float(rect[0]+(rect[2]/2)),float(rect[1]+(rect[3]/2)))
 
-#        print self.__center
+        xBound = rect[2]/2
+        yBound = rect[3]/2
 
-#        for point in points:
-#            self.__vectors.append(VectorFromPoints(self.__center, point))
+        if xBound < yBound:
+            radius = xBound
+        else:
+            radius = yBound
 
-#        print self.__vectors
+        delta = int(360/points)
 
-        self.__vectors.append(Vector(0,-1*random.randint(1,rect[3]/2)))
-        self.__vectors.append(Vector(random.randint(1,rect[2]/2),0))
-        self.__vectors.append(Vector(0,random.randint(1,rect[3]/2)))
-        self.__vectors.append(Vector(-1*random.randint(1,rect[2]/2),0))
+        angle = 0
+
+        doShortRadius = random.randint(1,100) > 75
+
+        didShortRadius = random.randint(2, points - 3)
+
+        for i in range(points):
+            angle = i * delta
+
+            spareRadius = radius
+            
+            if doShortRadius and didShortRadius > 0:
+                limiter = random.randint(2,3)
+                radius = radius * 1/limiter
+                didShortRadius -= 1
+
+            if angle >= 0 and angle <= 90:
+                newAngle = angle
+                if angle == 0:
+                    x = radius
+                    y = 0
+                elif angle == 90:
+                    x = 0
+                    y = radius
+                else:
+                    newAngle = math.radians(newAngle)
+                    x = abs(math.cos(newAngle)) * radius
+                    y = abs(math.sin(newAngle)) * radius
+            elif angle > 90 and angle <= 180:
+                newAngle = 180 - angle
+                if angle == 180:
+                    x = -1 * radius
+                    y = 0
+                else:
+                    newAngle = math.radians(newAngle)
+                    x = -1 * abs(math.cos(newAngle)) * radius
+                    y = abs(math.sin(newAngle)) * radius
+            elif angle > 180 and angle <= 270:
+                newAngle = 270 - angle
+                if angle == 270:
+                    x = 0
+                    y = -1 * radius
+                else:
+                    newAngle = math.radians(newAngle)
+                    x = -1 * abs(math.sin(newAngle)) * radius
+                    y = -1 * abs(math.cos(newAngle)) * radius
+            else:
+                newAngle = 360-angle
+                if angle == 360:
+                    x = radius
+                    y = 0
+                else:
+                    newAngle = math.radians(newAngle)
+                    x = abs(math.cos(newAngle)) * radius
+                    y = -1 * abs(math.sin(newAngle)) * radius
+
+            self.__vectors.append(Vector(x, y))
+            radius = spareRadius
 
         self.__centerVector = VectorFromPoints((rect[0],rect[1]), self.__center)
-
-#        print self.__centerVector
 
     # End __init__
 
